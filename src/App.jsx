269d556baceee405b2e70956efe1a7a4d027c547
dive0 <zhengshiwei5@gmail.com>
2023-03-26 19:09:32 -0400

    Create search to filter result

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import HolidayInfo from "./Components/HolidayInfo";
 
 function App() {
   const [list, setList] = useState([]);
+  const [filteredResult, setFilteredResult] = useState([]);
+  const [searchDate, setSearchDate] = useState("");
+  const [searchChoice, setSearchChoice] = useState("");
 
   useEffect(() => {
     const fetchAllHolidays = async () => {
@@ -17,22 +20,96 @@ function App() {
     fetchAllHolidays().catch(console.error);
   }, []);
 
+  const searchItems = (e) => {
+    const searchDateValue = document.getElementById("date").value;
+    const searchChoiceValue = document.getElementById("choice").value;
+    setSearchDate(searchDateValue);
+    setSearchChoice(searchChoiceValue);
+    if (searchDateValue !== "" || searchChoiceValue !== "") {
+      const filteredData = list.filter((item) => {
+        if (
+          item.weekday.date.name.includes(searchChoiceValue) &&
+          item.date.includes(searchDateValue)
+        ) {
+          return item;
+        } else if (
+          searchDateValue === "" &&
+          item.weekday.date.name.includes(searchChoiceValue)
+        ) {
+          return item.weekday.date.name.includes(searchChoiceValue);
+        } else if (
+          searchChoiceValue === "" &&
+          item.date.includes(searchDateValue)
+        ) {
+          return item.date.includes(searchDateValue);
+        }
+      });
+      setFilteredResult(filteredData);
+    } else {
+      setFilteredResult(list);
+    }
+  };
+
   return (
     <div className="App">
       <h1>2022 Holidays</h1>
 
+      <form>
+        <input
+          type="date"
+          min="2022-01-01"
+          max="2022-12-31"
+          id="date"
+          name="date"
+        />
+        <select name="weekday" id="choice">
+          <option value="">Select</option>
+          <option value="Monday">Monday</option>
+          <option value="Tuesday">Tuesday</option>
+          <option value="Wednesday">Wednesday</option>
+          <option value="Thursday">Thursday</option>
+          <option value="Friday">Friday</option>
+          <option value="Saturday">Saturday</option>
+          <option value="Sunday">Sunday</option>
+        </select>
+        <button type="button" onClick={searchItems}>
+          Submit
+        </button>
+      </form>
+
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Date</th>
-          <th>Observed Date</th>
-          <th>Weekday</th>
-          <th>Public</th>
-        </tr>
-        {list &&
-          list.map((holiday) => (
-            <HolidayInfo name={holiday.name} date={holiday.date} observedDate={holiday.observed} weekday={holiday.weekday.date.name} public={holiday.public}/>
-          ))}
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Observed Date</th>
+            <th>Weekday</th>
+            <th>Public</th>
+          </tr>
+        </thead>
+        {searchDate !== "" || searchChoice !== ""
+          ? filteredResult &&
+            filteredResult.map((holiday) => (
+              <HolidayInfo
+                name={holiday.name}
+                date={holiday.date}
+                observedDate={holiday.observed}
+                weekday={holiday.weekday.date.name}
+                public={holiday.public}
+                key={holiday.name}
+              />
+            ))
+          : list &&
+            list.map((holiday) => (
+              <HolidayInfo
+                name={holiday.name}
+                date={holiday.date}
+                observedDate={holiday.observed}
+                weekday={holiday.weekday.date.name}
+                public={holiday.public}
+                key={holiday.name}
+              />
+            ))}
       </table>
     </div>
   );
--- src/Components/HolidayInfo.jsx
+++ src/Components/HolidayInfo.jsx
@@ -1,12 +1,14 @@
 const HolidayInfo = (props) => {
   return (
-    <tr>
-      <td>{props.name}</td>
-      <td>{props.date}</td>
-      <td>{props.observedDate}</td>
-      <td>{props.weekday}</td>
-      <td>{props.public ? "Yes" : "No"}</td>
-    </tr>
+    <tbody>
+      <tr>
+        <td>{props.name}</td>
+        <td>{props.date}</td>
+        <td>{props.observedDate}</td>
+        <td>{props.weekday}</td>
+        <td>{props.public ? "Yes" : "No"}</td>
+      </tr>
+    </tbody>
   );
 };
 
