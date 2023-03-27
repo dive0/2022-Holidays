8596670a99d14f5cc3e1716a5ff42f86d1a979c7
dive0 <zhengshiwei5@gmail.com>
2023-03-26 22:37:00 -0400

    Add summary data along with its style

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,41 @@ function App() {
     fetchAllHolidays().catch(console.error);
   }, []);
 
+  const totalHolidays = list.length;
+
+  const median = () => {
+    let count = 0;
+    const middle = Math.floor(totalHolidays / 2);
+    let name;
+    list.map((holiday) => {
+      if (count === middle) {
+        name = holiday.name;
+      }
+      count++;
+    });
+    return name;
+  };
+
   return (
     <div className="App">
       <h1>2022 Holidays</h1>
+
+      <div className="summary">
+        <p>Total: {totalHolidays} Holidays</p>
+        <p>Median: {median()}</p>
+        {list &&
+          list.map((holiday, i) => {
+            if (i === 0) {
+              return (
+                <p key={holiday.date}>First Holiday Date: {holiday.date}</p>
+              );
+            } else if (i === totalHolidays - 1) {
+              return (
+                <p key={holiday.date}>Last Holiday Date: {holiday.date}</p>
+              );
+            }
+          })}
+      </div>
 
       <SearchForm
         setFilteredResult={setFilteredResult}
@@ -34,12 +66,12 @@ function App() {
         list={list}
       />
 
-        <DataTable
-          searchDate={searchDate}
-          searchChoice={searchChoice}
-          filteredResult={filteredResult}
-          list={list}
-        />
+      <DataTable
+        searchDate={searchDate}
+        searchChoice={searchChoice}
+        filteredResult={filteredResult}
+        list={list}
+      />
     </div>
   );
 }
